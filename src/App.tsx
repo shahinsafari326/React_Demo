1413a49bce2b3b12b9239ec262758295d13db463
shahin325-admin <shahin.safari1369@gmail.com>
2026-05-26 15:16:49 +0300

    files

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import ListGroup from "./components/ListGroup";
+
+
+function App () {
+    return <ListGroup />
+} 
+
+export default App;  
---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message () {
+      const name = "Mosh"
+
+    return <h1>Message {
+        name
+    }   </h1>
+}
+
+export default Message; 
